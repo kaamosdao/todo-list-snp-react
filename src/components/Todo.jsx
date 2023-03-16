1cd1161738f1b76eaf23c6ec36ec57cc428a8cbc
kaamosdao <kaamosdao@gmail.com>
2023-03-16 16:06:18 +0300

    Refactor Todo: fix linter errors

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
-import { removeTodo, updateTodo } from '../slices/todoReducer';
-import styles from './Todo.module.scss';
 
-const cn = classNames.bind(styles);
+import { removeTodo, updateTodo } from '../slices/todoReducer';
+
+import s from './Todo.module.scss';
+
+
+const cn = classNames.bind(s);
 
 const Todo = ({ id, title, checked }) => {
   const dispatch = useDispatch();
@@ -19,26 +23,33 @@ const Todo = ({ id, title, checked }) => {
   };
 
   return (
-    <li className={styles.todo}>
-      <div className={styles.container}>
-        <label className={styles.checkbox}>
+    <li className={s.todo}>
+      <div className={s.container}>
+        <label className={s.checkbox} htmlFor="todo">
           <input
             className={cn('visually-hidden', 'inputCheck')}
             type="checkbox"
+            id="todo"
             checked={checked}
             onChange={handleChecked}
           />
-          <span className={styles.checkboxMark}></span>
+          <span className={s.checkboxMark} />
         </label>
         <p className={cn('title', { 'todoCompleted': checked })}>{title}</p>
         <button
-          className={styles.buttonDelete}
+          className={s.buttonDelete}
           type="button"
           onClick={handleRemoveTodo}
-        ></button>
+        />
       </div>
     </li>
   );
+};
+
+Todo.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  checked: PropTypes.bool.isRequired,
 };
 
 export default Todo;
