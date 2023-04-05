f03c185bce596431da79705cbbdfffb787ec6614
kaamosdao <kaamosdao@gmail.com>
2023-04-05 13:40:00 +0300

    Add todoSelector: add memoized selectors

---
+++ src/slices/todoSelector.js
@@ -0,0 +1,33 @@
+import { createSelector } from '@reduxjs/toolkit';
+
+const selectTodoItems = createSelector(
+  (state) => state.todos.items,
+  (todos) => todos
+);
+
+export const selectTodoFilter = createSelector(
+  (state) => state.todos.filter,
+  (filter) => filter
+);
+
+export const selectActiveTodos = createSelector(
+  selectTodoItems,
+  (todos) => todos.filter((todo) => todo.status === 'active')
+);
+
+export const selectCompletedTodos = createSelector(
+  selectTodoItems,
+  (todos) => todos.filter((todo) => todo.status === 'completed')
+);
+
+export const selectTodosCount = createSelector(
+  selectTodoItems,
+  (todos) => todos.length
+);
+
+export const selectActiveTodosCount = createSelector(
+  selectActiveTodos,
+  (activeTodos) => activeTodos.length
+);
+
+export default selectTodoItems;
