e682c9ed5ced84425955e724e3c932204afcf912
kaamosdao <kaamosdao@gmail.com>
2023-04-05 17:03:51 +0300

    Refactor App: add InputRefContext.Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useRef } from 'react';
+
+import InputRefContext from './hooks/InputRefContext';
 
 import Main from './components/Main';
 
-const App = () => <Main />;
+const App = () => {
+  const inputRef = useRef(null);
+
+  return (
+    <InputRefContext.Provider value={inputRef}>
+      <Main />
+    </InputRefContext.Provider>
+  );
+};
 
 export default App;
