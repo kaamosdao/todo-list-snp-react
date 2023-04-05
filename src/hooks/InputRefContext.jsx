7e36d9941bef4ee00a7d0760e0488ebc803f4209
kaamosdao <kaamosdao@gmail.com>
2023-04-05 17:03:02 +0300

    Change hooks: add inputRefContext

---
+++ src/hooks/InputRefContext.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const InputRefContext = React.createContext({});
+
+export default InputRefContext;
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,7 +1,9 @@
 import { useContext } from 'react';
 
 import LocalStorageContext from './LocalStorageContext.jsx';
+import InputRefContext from './InputRefContext.jsx';
 
 const useLocalStorage = () => useContext(LocalStorageContext);
+export const useInputRef = () => useContext(InputRefContext);
 
 export default useLocalStorage;
