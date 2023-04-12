d3e54f4f6d20bd681374668030fd162d4feb5aae
kaamosdao <kaamosdao@gmail.com>
2023-04-12 14:02:43 +0300

    Refactor app: add localStorageMiddleware, remove useEffect localSorage

--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -1,9 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 
 import selectTodoItems, { selectTodoFilter } from '../slices/todoSelector';
 
-import { useLocalStorage } from '../hooks/index.js';
 import filters from '../types/types';
 
 import Todo from './Todo';
@@ -11,14 +10,8 @@ import Todo from './Todo';
 import s from './styles/TodosList.module.scss';
 
 const TodosList = () => {
-  const localStorageTodo = useLocalStorage();
-
   const todos = useSelector(selectTodoItems);
   const todoFilter = useSelector(selectTodoFilter);
-
-  useEffect(() => {
-    localStorageTodo.setData({ todos, todoFilter });
-  }, [todos, todoFilter, localStorageTodo]);
 
   return (
     <ul className={s.list}>
